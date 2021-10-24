3aa7d8b88f9c31bd61db68f310e68ad90551ab23
sopel1996 <sopel@live.ru>
2021-10-24 23:37:09 +0300

    mobile markup

--- media/js/index.js
+++ media/js/index.js
@@ -4,6 +4,35 @@ $(document).ready(function () {
         time: 2000
     });
 });
+
 $(window).scroll(function(){
     $('header').toggleClass('shrink', $(this).scrollTop() > 80);
 });
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+        button.toggleClass('burger-menu_button_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');

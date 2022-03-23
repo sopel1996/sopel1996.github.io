630f7ddffe53372bf678105479847986f30b549c
sopel1996 <sopel@live.ru>
2022-03-23 15:24:01 +0300

    update haval

--- haval/js/burger.js
+++ haval/js/burger.js
@@ -1,1 +1,28 @@
-{var l=".burger-menu";let e=document.querySelector(l),r=e.querySelector(".burger-menu_button"),t=e.querySelectorAll(".burger-menu_link"),n=e.querySelector(".burger-menu_overlay"),u=document.body;function c(){e.classList.toggle("burger-menu_active"),r.classList.toggle("burger-menu_button_active"),e.classList.contains("burger-menu_active")?u.style.overflow="hidden":u.style.overflow="visible"}r.addEventListener("click",e=>{e.preventDefault(),c()}),t.forEach(e=>{e.addEventListener("click",()=>c())}),n.addEventListener("click",()=>c())}
+function burgerMenu(selector) {
+    let menu = document.querySelector(selector);
+    let button = menu.querySelector('.burger-menu_button');
+    let links = menu.querySelectorAll('.burger-menu_link');
+    let overlay = menu.querySelector('.burger-menu_overlay');
+    let body = document.body;
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.forEach(el=>{el.addEventListener('click', () => toggleMenu())});
+    overlay.addEventListener('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.classList.toggle('burger-menu_active');
+        button.classList.toggle('burger-menu_button_active');
+
+        if (menu.classList.contains('burger-menu_active')) {
+            body.style.overflow =  'hidden';
+        } else {
+            body.style.overflow = 'visible';
+        }
+    }
+}
+
+burgerMenu('.burger-menu');

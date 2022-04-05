2aa4edddef69d558de70a63c0e25a4207a773351
sopel1996 <sopel@live.ru>
2022-04-05 19:49:14 +0300

    update ridge_landing
    add new page

--- ridge_landing/js/index.js
+++ ridge_landing/js/index.js
@@ -13,8 +13,21 @@ document.addEventListener("DOMContentLoaded", function () {
        })
    })
 
+   const anchors = document.querySelectorAll('a[href*="#"]')
 
-  
+    for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        const blockID = anchor.getAttribute('href').substr(1)
+        
+        document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+        })
+    })
+    }
+    
 
 
 

917cdb0f4ff50a783aa4a458c3d1cfeb6ba091c4
sopel1996 <sopelMurman@gmail.com>
2024-05-25 12:41:42 +0300

    add new amb

--- amb/js/index.js
+++ amb/js/index.js
@@ -18,50 +18,23 @@ document.addEventListener('DOMContentLoaded', function(){
   }
   burgerMenu('.burger-menu');
 
-  // const swiper = new Swiper('.sectionPopular_line_slider', {
-  //   // Optional parameters
-  //   direction: 'vertical',
-  //   loop: true,
-
-  //   // If we need pagination
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //   },
-
-  //   // Navigation arrows
-  //   navigation: {
-  //     nextEl: '.sectionPopular_line_sliderBtns_btn_prev',
-  //     prevEl: '.sectionPopular_line_sliderBtns_btn_next',
-  //   },
-
-  //   // And if we need scrollbar
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //   },
-  // });
   const swiperPopular = new Swiper('.sectionPopular_line_slider', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     spaceBetween: 20,
-    // If we need pagination
-    // Navigation arrows
     navigation: {
       nextEl: '.sectionPopular_line_sliderBtns_btn_next',
       prevEl: '.sectionPopular_line_sliderBtns_btn_prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
     breakpoints: {
-      // when window width is >= 320px
       376: {
         slidesPerView: 1,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 1.2,
       },
@@ -72,28 +45,22 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
   const swiperNews = new Swiper('.sectionNews_line_slider', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     spaceBetween: 20,
-    // If we need pagination
-    // Navigation arrows
     navigation: {
       nextEl: '.sectionNews_line_sliderBtns_btn_next',
       prevEl: '.sectionNews_line_sliderBtns_btn_prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
     breakpoints: {
-      // when window width is >= 320px
       580: {
         slidesPerView: 1.5,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 2.2,
       },
@@ -104,17 +71,14 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
   const swiperPartners = new Swiper('.sectionPartners_itemSlider', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
     spaceBetween: 20,
     breakpoints: {
-      // when window width is >= 320px
       580: {
         slidesPerView: 2,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 2.2,
       },
@@ -131,17 +95,14 @@ document.addEventListener('DOMContentLoaded', function(){
     
   });
   const swiperPartners_second = new Swiper('.sectionPartners_itemSlider_second', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1,
     spaceBetween: 20,
     breakpoints: {
-      // when window width is >= 320px
       580: {
         slidesPerView: 2,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 2.2,
       },
@@ -154,28 +115,22 @@ document.addEventListener('DOMContentLoaded', function(){
   });
 
   const swipernRecentlyViewed = new Swiper('.sectionRecentlyViewed_line_slider', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     spaceBetween: 20,
-    // If we need pagination
-    // Navigation arrows
     navigation: {
       nextEl: '.sectionRecentlyViewed_line_slider_btn_next',
       prevEl: '.sectionRecentlyViewed_line_slider_btn_prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
     breakpoints: {
-      // when window width is >= 320px
       580: {
         slidesPerView: 1.5,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 2.2,
       },
@@ -186,36 +141,80 @@ document.addEventListener('DOMContentLoaded', function(){
     }
   });
   const swiperAccessories = new Swiper('.sectionAccessories_line_slider', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     spaceBetween: 20,
-    // If we need pagination
-    // Navigation arrows
     navigation: {
       nextEl: '.sectionAccessories_line_slider_btn_next',
       prevEl: '.sectionAccessories_line_slider_btn_prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
     breakpoints: {
-      // when window width is >= 320px
       580: {
         slidesPerView: 1.5,
       },
-      // when window width is >= 480px
       780: {
         slidesPerView: 2.2,
       },
-      1360: {
+      1000: {
         slidesPerView: 3.2
+      },
+      1360: {
+        slidesPerView: 4.2
       }
 
     }
   });
+
+  var thumbSlider = new Swiper(".thumbSlider", {
+    spaceBetween: 10,
+    slidesPerView: 4.2,
+    freeMode: true,
+    watchSlidesProgress: true,
+    direction: 'horizontal',
+    breakpoints: {
+      501: {
+        direction: 'vertical'
+      },
+    }
+  });
+  var mainProductSlider = new Swiper(".mainProductSlider", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".mainProductSlider-button-next",
+      prevEl: ".mainProductSlider-button-prev",
+    },
+    thumbs: {
+      swiper: thumbSlider,
+    },
+  });
+
+
+  const tabs = document.querySelectorAll(".sectionCharacteristic_btns_btn");
+  const tabsContent = document.querySelectorAll(".sectionCharacteristic_content");
+  
+  for (let i = 0; i < tabs.length; i++){
+    tabs[i].onclick = () => {
+      changeTab(i);
+    };
+  }
+  function changeTab(index) {
+    console.log('tabs',tabs)
+    console.log('tabsContent',tabsContent)
+    for (let i=0; i<tabs.length; i++) {
+      tabs[i].classList.remove('active');
+    }
+    for (let i=0; i<tabsContent.length; i++) {
+      tabsContent[i].classList.remove('active');
+    }
+
+    tabs[index].classList.add('active');
+    tabsContent[index].classList.add('active');
+  }
+
 
 })
